77938c9e966502bc1c69e352788c5dbde421c7f5
Bruce Ruffin <bruffin1221@gmail.com>
2020-06-05 16:30:34 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -40,11 +40,12 @@ def call
   else "exit"
   end
 end
-end 
+end
 
 def list_songs
   Song.all.sort_by{|song| song.name}.each.with_index(1) do |song, index|
     puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    binding.pry
   end
 end
 
